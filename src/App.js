6b21384cd7b15df6cb0a7e98c7c115ac5926b485
Ian Danas <ianmdanas@gmail.com>
2023-05-30 19:24:32 -0700

    updating to be able to create rooms

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import ChatRoom from './pages/ChatRoom'
 import ChatHub from './pages/ChatHub'
 import NavTabs from './Components/NavTabs/index'
 import Home from './pages/Home'
+import {io} from 'socket.io-client'
+const URL = 'http://localhost:3001'
+const socket = io(URL)
 
 function App() {
   return (
     <Router>
       <NavTabs/>
       <Routes>
-        <Route path="/ChatRoom/:roomID" element={<ChatRoom/>} />
-        <Route path="/ChatRoom/" element={<ChatHub/>} />
+        <Route path="/ChatRoom/:roomID" element={<ChatRoom useSocket = {socket}/>} />
+        <Route path="/ChatRoom/" element={<ChatHub useSocket = {socket} />} />
         <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
