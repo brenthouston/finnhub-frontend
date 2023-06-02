7b0484a59f391594a112f31e27c3ef19f2e09075
Brent Houston <brentjustinhouston@gmail.com>
2023-06-02 13:30:17 -0700

    ticker search page done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ChatRoom from './pages/ChatRoom'
-import ChatHub from './pages/ChatHub'
-import NavTabs from './Components/NavTabs/index'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import {io} from 'socket.io-client'
+import ChatRoom from './pages/ChatRoom';
+import ChatHub from './pages/ChatHub';
+import NavTabs from './Components/NavTabs/index';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import {io} from 'socket.io-client';
 import Login from "./pages/Login";
 import background from "./images/finhubBG.png";
 import SignUp from "./pages/SignUp";
+import TickerSearch from "./pages/TickerSearch";
+
 
 import {useEffect} from 'react'
 const URL = 'http://localhost:3001'
@@ -30,6 +32,7 @@ function App() {
         <Route path="/profile" element={<Profile/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<SignUp/>} />
+        <Route path="/tickersearch" element={<TickerSearch/>} />
         <Route path="/" element={<Home/>} />
         <Route path="/chathub/" element={<ChatHub useSocket = {socket} />} />
         <Route path="/chatroom/:roomID" element={<ChatRoom useSocket = {socket}/>} />
