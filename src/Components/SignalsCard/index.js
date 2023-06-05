6140794539baabc5441e1762a547c09900e29b69
Brent Houston <brentjustinhouston@gmail.com>
2023-06-05 13:51:31 -0700

    card css on signals done

--- src/Components/SignalsCard/index.js
+++ src/Components/SignalsCard/index.js
@@ -6,11 +6,11 @@ import './style.css';
 export default function Card(props) {
 
   return (
-    <div className="card">
+    <div className="card" style={{border:"var(--bgGrn)"}}>
       <div className="card-header" style={{background:"var(--cardGrn)", color:"var(--primary)"}}>
         <h2>{props.heading}</h2>
       </div>
-      <div className="card-body">{props.children}</div>
+      <div className="card-body" style={{background:"var(--primary)"}}>{props.children}</div>
     </div>
   );
   
