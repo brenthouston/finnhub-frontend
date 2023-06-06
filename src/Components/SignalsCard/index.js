35e0aff628e51a2c2b1b91e47a7dd976946c6fb2
Brent Houston <brentjustinhouston@gmail.com>
2023-06-05 21:34:13 -0700

    signals page done

--- src/Components/SignalsCard/index.js
+++ src/Components/SignalsCard/index.js
@@ -8,7 +8,7 @@ export default function Card(props) {
   return (
     <div className="card" style={{border:"var(--bgGrn)"}}>
       <div className="card-header" style={{background:"var(--cardGrn)", color:"var(--primary)"}}>
-        <h2>{props.heading}</h2>
+        <h4 style={{fontSize:"1rem"}}>{props.heading}</h4>
       </div>
       <div className="card-body" style={{background:"var(--primary)"}}>{props.children}</div>
     </div>
