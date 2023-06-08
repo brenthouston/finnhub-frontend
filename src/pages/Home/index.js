970cac285e7cd0d3f70983b57f35dc2c09fdf124
Brent Houston <brentjustinhouston@gmail.com>
2023-06-07 20:37:26 -0700

    media q on home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -38,13 +38,13 @@ export default function Home() {
   }
 
   return (
-    <div className="container home">
-      <div className="row row-sm-12">
-        <section className="col m-5">
+    <div className="container home d-flex ">
+      <div className="row">
+        <section className="col-8">
           <h1 style={{fontWeight:"lighter"}}>Dashboard</h1>
           <h3>What is FinHub?</h3>
-          <div className="container w_card card">
-            <p style={{ padding: '25px 20px 0 20px'}}>
+          <div className="w_card card">
+            <p style={{ padding: '25px 20px 0 20px', fontSize:"clamp(12px 16px 18px) !important"}}>
               <strong>Welcome to FinHub</strong>, where finance meets community.
               Whether you're a seasoned trader or just starting your journey,
               our platform equips you with the tools, knowledge, and connections
@@ -58,9 +58,9 @@ export default function Home() {
             </ul>
           </div>
         </section>
+      <div className="col">
+        
       </div>
-      <div className="row">
-        <aside className="col m-5">
           <div className="user_search">
             <p style={{color: "#7f7c3d", fontSize: "22px"}}>Search for User</p>
             <div className="input-group input-group-lg">
@@ -68,15 +68,7 @@ export default function Home() {
             </div>
             <button onClick = {handleUserSearch} type="button" className="btn" style={{background: "#65293d", color: "#d8d1bc", display:"flex", padding:"0"}}>Search</button>
             </div>
-{/* 
-          <div className="ticker_search">
-            <p style={{color: "#7f7c3d", fontSize: "22px"}}>Search for Ticker</p>
-            <div className="input-group input-group-lg">
-            <input type="text" placeholder="Stock Symbol" className="inputGroup-sizing-lg"></input>
-            </div>
-            <button type="button" className="btn" style={{background: "#65293d", color: "#d8d1bc", display:"flex", padding:"0"}}>Search</button>
-          </div> */}
-        </aside>
+        
       </div>
       <p style={{color: "#7f7c3d", fontSize: "14px"}}>Disclaimer: Trading in equities is risky. Information provided on this website does not constitute investment advice. There is no guarantee of profits and we will not be responsible for any losses incurred or decisions made based on the information provided here. Past performance is not an indicator of future returns. </p>
       <Error errorMsg = {errorMsg} show = {show} setShow = {setShow}/>
