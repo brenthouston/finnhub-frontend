5cfdcb530e94a2f4e6ec529a11f212a766b36449
Brent Houston <brentjustinhouston@gmail.com>
2023-06-08 14:11:40 -0700

    dayjs chart feed

--- src/utils/ChartAPI.js
+++ src/utils/ChartAPI.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import dayjs from 'dayjs'
+
+
 
 async function getSearch(query) {
+let date = dayjs().format("YYYY-MM-DD")
 console.log(query);
 try{
-  const response = await axios.get(`https://api.polygon.io/v2/aggs/ticker/${query}/range/1/day/2022-05-05/2023-05-05?adjusted=true&sort=desc&limit=120&apiKey=${process.env.REACT_APP_CHART_API}`);  
+  const response = await axios.get(`https://api.polygon.io/v2/aggs/ticker/${query}/range/1/day/2022-05-05/${date}?adjusted=true&sort=desc&limit=120&apiKey=${process.env.REACT_APP_CHART_API}`);  
 console.log('API response: ',response);
 
 
